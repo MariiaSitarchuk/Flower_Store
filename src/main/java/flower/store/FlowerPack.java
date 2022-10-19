15751372dbe4e95433a0cb8dd8d624aacfa74005
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-19 16:16:36 +0300

    Update FlowerPack.java

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,6 +1,14 @@
-package flower.store;
+package flowerStore;
+
+/**
+* pack of the same flowers
+*/
 
 public class FlowerPack {
     private Flower flower;
-    private int quantity;
+    private int amount;
+
+    public int getPrice() {
+        return (flower.getPrice() * amount);
+    }
 }
