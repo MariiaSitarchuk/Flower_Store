d8d11e03ccdab2ac920c72e19c050c9889f4e700
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-20 20:34:31 +0300

    Create Chamomile.java

---
+++ src/main/java/flower/store/Chamomile.java
@@ -0,0 +1,10 @@
+package store;
+
+public class Chamomile extends Flower {
+    public static final int chamLen = 15;
+    public static final int chamPrice = 20;
+
+    public Chamomile() {
+        super(chamLen, FlowerColor.WHITE, chamPrice);
+    }
+}
