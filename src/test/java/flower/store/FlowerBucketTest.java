c0e82adde5b950656e06dd97fab1c9ad97d14e0d
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-20 21:05:06 +0300

    Update FlowerBucketTest.java

--- src/test/java/flower/store/FlowerBucketTest.java
+++ src/test/java/flower/store/FlowerBucketTest.java
@@ -10,22 +10,22 @@ public class FlowerBucketTest {
     private static final Random RANDOM_GENERATOR = new Random();
     private static final int MAX_QUANTITY = 1000;
     private static final int MAX_PRICE = 100;
-    private static final int price = RANDOM_GENERATOR.nextInt(MAX_PRICE);
-    private static final int quantity = RANDOM_GENERATOR.nextInt(MAX_QUANTITY);
+    private static final int RAN_PRICE = RANDOM_GENERATOR.nextInt(MAX_PRICE);
+    private static final int RAN_QUANTITY = RANDOM_GENERATOR.nextInt(MAX_QUANTITY);
 
     private FlowerBucket flowerBucket;
 
     @BeforeEach
     public void init() {
         Flower flower = new Rose();
-        flower.setPrice(price);
-        FlowerPack flowerPack = new FlowerPack(flower, quantity);
+        flower.setPrice(RAN_PRICE);
+        FlowerPack flowerPack = new FlowerPack(flower, RAN_QUANTITY);
         flowerBucket = new FlowerBucket();
         flowerBucket.add(flowerPack);
     }
 
     @Test
     public void testPrice() {
-        Assertions.assertEquals(price * quantity, flowerBucket.getPrice());
+        Assertions.assertEquals(RAN_PRICE * RAN_QUANTITY, flowerBucket.getPrice());
     }
 }
