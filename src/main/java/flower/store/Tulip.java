b705aab1c30721299e0caffe792f107c54afd3a1
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-20 20:50:43 +0300

    Create Tulip.java

---
+++ src/main/java/flower/store/Tulip.java
@@ -0,0 +1,10 @@
+package store;
+
+public class Tulip extends Flower {
+    public static final int tulipLen = 15;
+    public static final int tulipPrice = 40;
+
+    public Tulip() {
+        super(tulipLen, FlowerColor.BLUE, tulipPrice);
+    }
+}
