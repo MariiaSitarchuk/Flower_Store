45c3abaaaf4fc31345ab2ee0cca0cafd9169f029
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-20 20:47:32 +0300

    Create Rose.java

---
+++ src/main/java/flower/store/Rose.java
@@ -0,0 +1,10 @@
+package store;
+
+public class Rose extends Flower {
+    public static final int roseLen = 25;
+    public static final int rosePrice = 60;
+
+    public Rose() {
+        super(roseLen, FlowerColor.RED, rosePrice);
+    }
+}
