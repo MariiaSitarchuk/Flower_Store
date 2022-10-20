360cb9a6b0f6e1c4cf031bc8244c7615b87a9148
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-20 21:03:08 +0300

    Update Rose.java

--- src/main/java/flower/store/Rose.java
+++ src/main/java/flower/store/Rose.java
@@ -1,10 +1,10 @@
 package store;
 
 public class Rose extends Flower {
-    public static final int roseLen = 25;
-    public static final int rosePrice = 60;
+    public static final int ROSE_LEN = 25;
+    public static final int ROSE_PRICE = 60;
 
     public Rose() {
-        super(roseLen, FlowerColor.RED, rosePrice);
+        super(ROSE_LEN, FlowerColor.RED, ROSE_PRICE);
     }
 }
