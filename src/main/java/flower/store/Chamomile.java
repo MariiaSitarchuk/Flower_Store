d5f532faccd7aa65aa96896c36d49382c56f2542
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-20 21:02:34 +0300

    Update Chamomile.java

--- src/main/java/flower/store/Chamomile.java
+++ src/main/java/flower/store/Chamomile.java
@@ -1,10 +1,10 @@
 package store;
 
 public class Chamomile extends Flower {
-    public static final int chamLen = 15;
-    public static final int chamPrice = 20;
+    public static final int CHAM_LEN = 15;
+    public static final int CHAM_PRICE = 20;
 
     public Chamomile() {
-        super(chamLen, FlowerColor.WHITE, chamPrice);
+        super(CHAM_LEN, FlowerColor.WHITE, CHAM_PRICE);
     }
 }
