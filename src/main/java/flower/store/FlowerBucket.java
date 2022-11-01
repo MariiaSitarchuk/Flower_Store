0f1e499ae6752231287e2cd0ea8cda1a55a9269c
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-11-01 13:59:52 +0200

    Update FlowerBucket.java

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -6,15 +6,19 @@ import java.util.ArrayList;
 
 public class FlowerBucket {
     //private int amount;
-    private final ArrayList<FlowerPack> packList = new ArrayList<FlowerPack>();
+    private final ArrayList<FlowerPack> packList;
+    
+     public FlowerBucket() {
+        this.packList = new ArrayList<FlowerPack>();
+    }
 
     public void add(FlowerPack newPack) {
-        packList.add(newPack);
+        this.packList.add(newPack);
     }
 
     public double getPrice() {
         double sum = 0.0;
-        for (FlowerPack pack : packList) {
+        for (FlowerPack pack : this.packList) {
             sum = sum + pack.getPrice();
         }
         return sum;
